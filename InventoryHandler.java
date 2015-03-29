83e38402d2ebb3997d11939f8d3dc043a989568f
res204 <Ryan@nbp--27-210.nbp.ruw.rutgers.edu>
2015-03-28 20:41:41 -0400

    Comments

--- InventoryHandler.java
+++ InventoryHandler.java
@@ -1,11 +1,23 @@
+/**
+ * InventoryHandler.java
+ * Author: Ryan Sanichar
+ * 
+ * Handler for the InventoryWindow.java file
+ * Generates ingredients to be used as the inventory. 
+ */
+
+
 package Manager;
 import ADT.Ingredient;
 
 
 public class InventoryHandler {
 	
+	
+// The ingredient list
 public Ingredient IngredientList[];
 	
+	// Make the ingredients, give them a name, and a quantity
 	public Ingredient[] AllIngredient=
 		{
 			new Ingredient("Tomatoes",100),
@@ -14,6 +26,7 @@ public Ingredient IngredientList[];
 			new Ingredient("Chicken",100),
 		};
 	
+	// Query the ingredient
 	public void QueryIngredient()
 	{
 		IngredientList = AllIngredient;
