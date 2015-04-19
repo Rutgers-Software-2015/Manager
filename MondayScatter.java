a1c846e483875742b70da4935bcde32edccf7c1d
Harsh Shah <hms96@scarletmail.rutgers.edu>
2015-04-19 02:37:31 -0400

    ddf

--- MondayScatter.java
+++ MondayScatter.java
@@ -1,7 +1,7 @@
 package Manager;
 
 import javafx.application.Platform;
-import javafx.beans.property.SimpleListProperty;
+
 import javafx.embed.swing.JFXPanel;
 import javafx.scene.Scene;
 import javafx.scene.chart.*;
