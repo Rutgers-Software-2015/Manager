28ce13fc243b4c77be690732c94e09341c503bf7
Harsh Shah <hms96@scarletmail.rutgers.edu>
2015-04-18 22:10:34 -0400

    installed fx

--- GraphGenerator.java
+++ GraphGenerator.java
@@ -2,44 +2,12 @@ package Manager;
 
 import javafx.*;
 
-
-import java.awt.*; 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.UnknownHostException;
-import java.text.DateFormat;
-import java.text.DecimalFormat;
-import java.util.Collections;
-import java.util.Date;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
-import java.util.Vector;
-
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JDialog;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTabbedPane;
-import javax.swing.JTable;
-import javax.swing.JTextPane;
-import javax.swing.ScrollPaneConstants;
-import javax.swing.SwingConstants;
-import javax.swing.Timer;
-import javax.swing.border.BevelBorder;
-import javax.swing.table.DefaultTableCellRenderer;
-import javax.swing.table.DefaultTableModel;
+import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
 
 
 //import Login.LoginWindow;
@@ -149,6 +117,7 @@ public class GraphGenerator{
 		}
 		
 		
+		
 	}
 	
 	public int findItemIndex(String I)
@@ -165,7 +134,7 @@ public class GraphGenerator{
 
 	public void Gen_ItemBarGraph()
 	{
-		
+		organizeData();
 	}
 	
 }
