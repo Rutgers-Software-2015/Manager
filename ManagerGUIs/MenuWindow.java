d4a00a3080beb48c3f7107b9633e73fb029ff145
thetandoori <rahul@Rahuls-MacBook-Pro.local>
2015-04-29 17:31:40 -0400

    header manager

--- ManagerGUIs/MenuWindow.java
+++ ManagerGUIs/MenuWindow.java
@@ -11,11 +11,6 @@
  */
 
 package Manager.ManagerGUIs;
-/*
-@author Ryan Sanichar
-@tester Ryan Sanichar
-@debugger Ryan Sanichar
-*/
 import java.awt.*;   
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
